c7589c52630dbc1332a021e664178ead8b4976d3
alagha-go <abdihakimosman1480@gmail.com>
2022-06-03 07:52:42 +0000

    created method to save Secret

--- identity/main.go
+++ identity/main.go
@@ -3,6 +3,7 @@ package identity
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 )
 
 
@@ -13,4 +14,12 @@ var (
 func LoadSecret() {
 	data, _ := ioutil.ReadFile("./secret.json")
 	json.Unmarshal(data, &SecretData)
+}
+
+func (Secret *Secret) Save() {
+	data, err := json.Marshal(Secret)
+	if err != nil {
+		log.Panic(err)
+	}
+	ioutil.WriteFile("./secret.json", data, 0755)
 }
