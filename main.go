c2211ac0c867f9bd47d07bf57da4ac103ce9f017
alagha-go <abdihakimosman1480@gmail.com>
2022-06-03 07:43:37 +0000

    created Secret struct

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"citizenship/crawler"
+	"citizenship/identity"
 	"time"
 )
 
@@ -10,8 +11,15 @@ var (
 	Cookies string
 )
 
+type Secret struct {
+	MongoDBUrl							string							`json:"mongodb_url,omitempty" bson:"mongodb_url,omitempty"`
+	CurrentIDNumber						int								`json:"current_id_number,omitempty" bson:"current_id_number,omitempty"`
+}
+
 func main() {
-	
+	go SetCookies()
+	time.Sleep(5*time.Second)
+	identity.GetID("Doe", 8, Token, Cookies)
 }
 
 func SetCookies() {
