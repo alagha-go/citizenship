6720839c6cbadc69c26e31478a5ab8575b765c64
alagha-go <abdihakimosman1480@gmail.com>
2022-06-03 08:07:57 +0000

    added ID field to the Identityt struct

--- identity/structs.go
+++ identity/structs.go
@@ -1,7 +1,10 @@
 package identity
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 
 type Identity struct {
+	ID									primitive.ObjectID			`json:"_id,omitempty" bson:"_id,omitempty"`
 	Name        						string 						`json:"name" bson:"name"`       
 	Status      						string 						`json:"status" bson:"status"`     
 	Citizenship 						string 						`json:"citizenship" bson:"citizenship"`
