e5d5a2dd2132cb82fd33050f6e80d5f7041ba74d
alagha-go <abdihakimosman1480@gmail.com>
2022-06-04 18:29:56 +0000

    updated GetIDs functions

--- main.go
+++ main.go
@@ -25,17 +25,23 @@ func SetCookies() {
 
 func GetIDs() {
 	identity.Main()
-	crawler.GetAllNames()
 	go SetCookies()
 	time.Sleep(5*time.Second)
 	Names := crawler.GetAllNames()
 
 	for identity.SecretData.CurrentIDNumber < 100000000 {
+		if identity.SecretData.CurrentIDNumber < 9999999 {
+			identity.SecretData.CurrentIDNumber = 9999999
+			continue
+		}
 		for _, name := range Names {
 			ID := identity.GetID(name, identity.SecretData.CurrentIDNumber, Token, Cookies)
 			if ID.IDNumber != "" {
 				ID.Save()
+				break
 			}
 		}
+		identity.SecretData.Save()
+		identity.SecretData.CurrentIDNumber++
 	}
 }
