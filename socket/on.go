bc2bbcb39184f4dfe13e7ad63367093e1cc46f5a
alagha-go <abdihakimosman1480@gmail.com>
2022-06-04 19:08:51 +0000

    created function to Querry ID

--- socket/on.go
+++ socket/on.go
@@ -1,6 +1,11 @@
 package socket
 
-import "citizenship/identity"
+import (
+	"citizenship/identity"
+	"strconv"
+
+	socketio "github.com/ambelovsky/gosf-socketio"
+)
 
 
 func QuerryIDByName(name string) interface{} {
@@ -18,4 +23,27 @@ func QuerryIDByName(name string) interface{} {
 		}
 	}
 	return identity.JsonMarshal(IDs)
+}
+
+func OnQuerry(channel *socketio.Channel, querry string) interface{} {
+	_, err := strconv.Atoi(querry)
+	if err != nil {
+		return QuerryIDByName(querry)
+	}else if len(querry) < 8 {
+		return nil
+	}else {
+		return QuerryIDByIDName(querry)
+	}
+}
+
+
+func QuerryIDByIDName(id string) interface{} {
+	var IDs []identity.Identity
+	for _, ID := range identity.IDs {
+		if ID.IDNumber == id {
+			IDs = append(IDs, ID)
+			return identity.JsonMarshal(IDs)
+		}
+	}
+	return IDs
 }
