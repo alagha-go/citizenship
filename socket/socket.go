efa55299ab1f558aade7bfd301157af6fbf0782a
alagha-go <abdihakimosman1480@gmail.com>
2022-06-04 18:44:29 +0000

    created function to check on connection

--- socket/socket.go
+++ socket/socket.go
@@ -1,8 +1,10 @@
 package socket
 
 import (
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	socketio "github.com/ambelovsky/gosf-socketio"
 	"github.com/ambelovsky/gosf-socketio/transport"
@@ -19,4 +21,29 @@ func StartSocketServer() {
 
 	log.Println("Starting SocketServer...")
 	log.Panic(http.ListenAndServe(PORT, serveMux))
+}
+
+
+func OnConnection(channel *socketio.Channel) {
+	if len(channel.Request().Header["Cookie"]) <= 0  {
+		channel.Emit("password", `{"error": "unauthorized"`)
+		time.Sleep(800*time.Millisecond)
+		channel.Close()
+		return
+	}
+	data, err := ioutil.ReadFile("./password.txt")
+	if err != nil {
+		channel.Emit("password", `{"error": "could not read password from our database"`)
+		time.Sleep(800*time.Millisecond)
+		channel.Close()
+		return
+	}
+	password := string(data)
+	cookie := channel.Request().Header["Cookie"][0]
+	if password != cookie {
+		channel.Emit("password", `{"error": "wrong password"`)
+		time.Sleep(800*time.Millisecond)
+		channel.Close()
+		return
+	}
 }
