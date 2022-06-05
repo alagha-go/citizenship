3cf0dd0acefe394980744ecef4464ab7492361b7
alagha-go <abdihakimosman1480@gmail.com>
2022-06-05 17:54:49 +0000

    updated function to querryIDBYName

--- socket/on.go
+++ socket/on.go
@@ -3,23 +3,41 @@ package socket
 import (
 	"citizenship/identity"
 	"strconv"
+	"strings"
 
 	socketio "github.com/ambelovsky/gosf-socketio"
 )
 
 
 func QuerryIDByName(name string) interface{} {
+	Names := strings.Split(name, " ")
 	var IDs []identity.Identity
-	for _, ID := range identity.IDs {
-		if ID.FirstName == name {
-			IDs = append(IDs, ID)
-			continue
-		}else if ID.Name == name {
-			IDs = append(IDs, ID)
-			continue
-		}else if ID.OtherName == name {
-			IDs = append(IDs, ID)
-			continue
+	if len(Names) == 1 {
+		for _, ID := range identity.IDs {
+			if ID.FirstName == name {
+				IDs = append(IDs, ID)
+				continue
+			}else if ID.Surname == name {
+				IDs = append(IDs, ID)
+				continue
+			}else if ID.OtherName == name {
+				IDs = append(IDs, ID)
+				continue
+			}
+		}
+	}else if len(Names) == 2 {
+		for _, ID := range identity.IDs {
+			if name == ID.Name {
+				IDs = append(IDs, ID)
+			}else if Names[0] == ID.FirstName && Names[1] == ID.OtherName {
+				IDs = append(IDs, ID)
+			}
+		} 
+	}else if len(Names) == 3 {
+		for _, ID := range identity.IDs {
+			if Names[0] == ID.FirstName && Names[1] == ID.OtherName && Names[2] == ID.Surname {
+				IDs = append(IDs, ID)
+			}
 		}
 	}
 	return identity.JsonMarshal(IDs)
